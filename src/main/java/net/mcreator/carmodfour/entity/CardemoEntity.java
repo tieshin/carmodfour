9c2de61a2fa848277213b0380985a93346d9b13c
tieshin <tieshintekashi@gmail.com>
2025-10-21 17:00:01 -0400

    HEADLIGHT BEAMS PART 2 FINAL PATCH

--- src/main/java/net/mcreator/carmodfour/entity/CardemoEntity.java
+++ src/main/java/net/mcreator/carmodfour/entity/CardemoEntity.java
@@ -197,15 +197,15 @@ public class CardemoEntity extends Mob implements IAnimatable {
 
         // Beam and brightness per mode
         final int targetLightLevel = switch (mode) {
-            case 1 -> 7;
-            case 2 -> 12;
+            case 1 -> 5;
+            case 2 -> 10;
             case 3 -> 15;
             default -> 0;
         };
         final int beamDistance = switch (mode) {
-            case 1 -> 3;
-            case 2 -> 5;
-            case 3 -> 8;
+            case 1 -> 5;
+            case 2 -> 10;
+            case 3 -> 15;
             default -> 0;
         };
 
